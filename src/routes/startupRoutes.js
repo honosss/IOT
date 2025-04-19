2607e0f81590157e78e88ceb8a34ff45bb24bd0b
HT <nguyenhoangtu99@gmail.com>
2025-04-20 00:20:16 +0700

    chang location

--- src/routes/startupRoutes.js
+++ src/routes/startupRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { handleStartupMessage } = require('../../controllers/startupController');
+const { handleStartupMessage } = require('../controllers/startupController');
 
 const router = express.Router();
 
