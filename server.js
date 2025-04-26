af3f6ee5f72448eea41e195e677947500b9a4ae7
HT <nguyenhoangtu99@gmail.com>
2025-04-26 17:01:30 +0700

    update-log

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config()
 const mqttService = require('./src/services/mqttService');
+const logger = require('./src/utils/logger');
 
 // 1️⃣ Kết nối MongoDB
 mongoose.connect(process.env.MONGO_URI)
 
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.error("MongoDB connection error:", err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => logger.info("MongoDB connected"))
+  .catch(err => logger.error("MongoDB connection error:" + err));
 
 
 // Khởi động MQTT Service
